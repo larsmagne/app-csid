e252482157dd41c62444fad2385cb724eb8f11ab
Lars Magne Ingebrigtsen <larsi@install64>
2015-09-09 15:47:23 +0200

    Start implementing an offline cache

--- www/index.js
+++ www/index.js
@@ -37,21 +37,72 @@ function splash() {
 var base = "http://csid.no";
 
 function loadData() {
-  $.ajax({
-    url: "http://csid.no/index.html",
-    dataType: "text",
-    success: function(data) {
-      var div = document.createElement("div");
-      div.innerHTML = data;
-      $(div).find("script").remove();
-      $(div).find("head").remove();
-      document.body.innerHTML = "";
-      document.body.appendChild(div);
-      addNavigation();
-    },
-    error: function(error) {
-      console.log(error);
-    }
+  if (navigator.network.connection.type === Connection.UNKNOWN ||
+      navigator.network.connection.type === Connection.NONE) {
+    window.requestFileSystem(
+      LocalFileSystem.PERSISTENT, 0,
+      function(fileSystem) {
+        fileSystem.root.getFile(
+	  "cache.html", null,
+	  function(fileEntry) {
+	    fileEntry.file(
+	      function(file) {
+		var reader = new FileReader();
+		reader.onloadend = function(evt) {
+		  displayCache(evt.target.result);
+		};
+		reader.readAsText(file);
+	      },		
+	      displayCacheFailure);
+	  },
+	  displayCacheFailure);
+      },
+      displayCacheFailure
+    );
+  } else {
+    $.ajax({
+      url: "http://csid.no/index.html",
+      dataType: "text",
+      success: function(data) {
+	var div = document.createElement("div");
+	div.innerHTML = data;
+	$(div).find("script").remove();
+	$(div).find("head").remove();
+	document.body.innerHTML = "";
+	document.body.appendChild(div);
+	addNavigation();
+      },
+      error: function(error) {
+	console.log(error);
+      }
+    });
+  }
+}
+
+function displayCache(text) {
+  alert("success");
+  var div = document.createElement("div");
+  div.innerHTML = text;
+  document.body.innerHTML = "";
+  document.body.appendChild(div);
+  addNavigation();
+  $.colorbox({html: "<div class='venue-top'>Device offline; showing cached data</div>",
+	      width: $(window).width() + "px",
+	      close: "Close",
+	      transition: "none",
+	      className: "event-lightbox"});
+}
+
+function displayCacheFailure() {
+  alert("failure");
+  $.colorbox({html: "<div class='venue-top'>Device offline and no cache available</div><a href='#' id='csid-close'>Close</a>",
+	      width: $(window).width() + "px",
+	      closeButton: false,
+	      transition: "none",
+	      className: "event-lightbox"});
+  $("#csid-close").bind("click", function() {
+    navigator.app.exitApp();
+    return false;
   });
 }
 
