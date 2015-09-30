b6a2dd32c17c474ff8ded467d8af426042841233
Lars Magne Ingebrigtsen <larsi@install64>
2015-09-30 17:42:08 +0200

    Hide the loading of the font.

--- www/index.js
+++ www/index.js
@@ -63,11 +63,15 @@ function loadData() {
 	div.innerHTML = data;
 	$(div).find("script").remove();
 	$(div).find("head").remove();
-	document.body.innerHTML = "";
-	document.body.appendChild(div);
-	addNavigation();
-	saveCache(data);
-	loadTime = Date.now();
+	waitForWebfonts(
+	  ["SourceSans"],
+	  function() {
+	    document.body.innerHTML = "";
+	    document.body.appendChild(div);
+	    addNavigation();
+	    saveCache(data);
+	    loadTime = Date.now();
+	  });
       },
       error: function(error) {
 	loadCache();
@@ -148,8 +152,63 @@ function saveCache(data) {
     fail);
 }
 
+function waitForWebfonts(fonts, callback) {
+  var loadedFonts = 0;
+  for(var i = 0, l = fonts.length; i < l; ++i) {
+    (function(font) {
+      var node = document.createElement('span');
+      // Characters that vary significantly among different fonts
+      node.innerHTML = 'giItT1WQy@!-/#';
+      // Visible - so we can measure it - but not on the screen
+      node.style.position      = 'absolute';
+      node.style.left          = '-10000px';
+      node.style.top           = '-10000px';
+      // Large font size makes even subtle changes obvious
+      node.style.fontSize      = '300px';
+      // Reset any font properties
+      node.style.fontFamily    = 'sans-serif';
+      node.style.fontVariant   = 'normal';
+      node.style.fontStyle     = 'normal';
+      node.style.fontWeight    = "bold";
+      node.style.letterSpacing = '0';
+      document.body.appendChild(node);
+
+      // Remember width with no applied web font
+      var width = node.offsetWidth;
+
+      node.style.fontFamily = font;
+
+      var interval;
+      var checkFont = function() {
+        // Compare current width with original width
+        if(node && node.offsetWidth != width) {
+          ++loadedFonts;
+          node.parentNode.removeChild(node);
+          node = null;
+        }
+
+        // If all fonts have been loaded
+        if (loadedFonts >= fonts.length) {
+          if (interval) {
+            clearInterval(interval);
+          }
+          if (loadedFonts == fonts.length) {
+            callback();
+            return true;
+          }
+        }
+	return false;
+      };
+
+      if(!checkFont()) {
+        interval = setInterval(checkFont, 50);
+      }
+    })(fonts[i]);
+  }
+}
+
 function fail() {
-  alert("failed");
+  alert("Failed saving the cache");
 }
 
 document.addEventListener("deviceready", splash, false);
